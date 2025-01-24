ffe6f918fccfce5562122f42c3cedcbcb707d046
Mario Eduardo Quiroga Bernal <marioeduardoquirogaber@gmail.com>
2025-01-24 14:27:26 -0600

    Subiendo el codigo con los errores solucionados

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-*Ejercicio numero 3
+# Ejercicio numero 3 - revisando codigo de otros
+* Se ha echo que se muestren los productos
+* Se ha añadido un estilo css al titulo del articulo
+* Se ha permitido permitir la busqueda tanto en mayusculas como en minusculas
+* Se ha añadido un README.md
+
+Modificated with love by: Mario Eduardo Quiroga Bernal
--- filtro.js
+++ filtro.js
@@ -8,37 +8,37 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+const li = document.getElementById("lista-de-productos") // Obtenemos el objeto por id
+const input = document.querySelector('input'); // hacemos que selecione el input quitando el . de .input y cambiamos su nombre a "input"
 
 for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
+  var div = document.createElement("div") // Cambiamos el nobre de la variable a "div"
+  div.classList.add("producto")
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
+  var parrafo = document.createElement("p")
+  parrafo.classList.add("titulo") // No existe la clase titulo en css, asi que la creamos
+  parrafo.textContent = productos[i].nombre
   
   var imagen = document.createElement("img");
   imagen.setAttribute('src', productos[i].img);
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
+  div.appendChild(parrafo)
+  div.appendChild(imagen)
 
-  li.appendChild(d)
+  li.appendChild(div)
 }
 
-displayProductos(productos)
-const botonDeFiltro = document.querySelector("button");
+//displayProductos(productos) // No es necesaria la funcion asi que la comentamos
+const botonDeFiltro = document.getElementById("button"); // Creamos un id en el boton y lo seleccionamos por el
 
 botonDeFiltro.onclick = function() {
   while (li.firstChild) {
     li.removeChild(li.firstChild);
   }
 
-  const texto = $i.value;
+  const texto = input.value;
   console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
+  const productosFiltrados = filtrado(productos, texto);
 
   for (let i = 0; i < productosFiltrados.length; i++) {
     var d = document.createElement("div")
@@ -59,5 +59,6 @@ botonDeFiltro.onclick = function() {
 }
 
 const filtrado = (productos = [], texto) => {
+  texto = texto.toLowerCase() // antes de verificar se hace minusculas
   return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
 }  
